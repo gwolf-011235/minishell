5fb232bc9c435478e71461dcfe6b9bf07772023e
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-17 18:23:12 +0200

    feat(minishell.h): add extern environ + hashtable

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:20:30 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/12 15:22:26 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/17 17:46:05 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@
 # include <stdlib.h>
 
 # include "libft.h"
+# include "hashtable.h"
+
+extern char	**environ;
 
 #endif
