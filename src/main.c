f649d441fe346d87b7e32f8ad1db1bc5039e683c
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-17 18:26:35 +0200

    feat(main): trying out environment

--- src/main.c
+++ src/main.c
@@ -6,25 +6,22 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:15:13 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/12 17:26:01 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/17 17:58:42 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char *argv[], char *envp[])
+int	main(void)
 {
-	int		i;
-	char	*env_ptr;
+	int			i;
+	char		*env_ptr;
+	t_hashtable	*ht;
 
-	(void)argc;
-	(void)argv;
 	i = 0;
-	env_ptr = envp[i];
-	while (env_ptr)
-	{
-		printf("%s\n", env_ptr);
-		i++;
-		env_ptr = envp[i];
-	}
+	env_ptr = environ[i];
+	ht = ft_hashtable_create(128, ft_hash_fnv1);
+	size_t keylen = ft_strchr(env_ptr, '=') - env_ptr;
+	ft_hashtable_insert(ht, env_ptr, keylen);
+	ft_hashtable_print(ht);
 }
