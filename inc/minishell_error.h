90aa07f1375938535f4c617f87e6c097c83f962e
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-24 12:44:03 +0200

    feat(minishell_error): new h file to collect error code

---
+++ inc/minishell_error.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_error.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 11:58:28 by gwolf             #+#    #+#             */
+/*   Updated: 2023/05/24 12:36:15 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_ERROR_H
+# define MINISHELL_ERROR_H
+
+typedef enum e_error {
+	SUCCESS,
+	ERR_MALLOC,
+	ERR_UNDEFINED,
+	ERR_HT_NO_INSERT,
+	ERR_HT_NO_DELETE,
+	ERR_EMPTY
+}	t_error;
+
+
+#endif
