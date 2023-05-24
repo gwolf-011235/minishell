f7566c8bd21622affbde95408c18c2b1060a73bc
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-24 11:50:02 +0200

    fix(ht_gen): add comments

--- src/hashtable_generate.c
+++ src/hashtable_generate.c
@@ -6,13 +6,20 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:13:36 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/19 11:22:32 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/24 11:49:22 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hashtable.h"
 #include "libft.h"
 
+/**
+ * @brief Implements the FNV (Fowler-Noll-Vo)-1 hash function
+ * FNV_OFFSET and FNV_PRIME are defined in hashtable.h
+ * @param string The string to generate a hash from
+ * @param len Length of the string
+ * @return uint64_t Hash value of string
+ */
 uint64_t	ft_hash_fnv1(const char *string, size_t len)
 {
 	uint64_t	hash_value;
@@ -29,6 +36,15 @@ uint64_t	ft_hash_fnv1(const char *string, size_t len)
 	return (hash_value);
 }
 
+/**
+ * @brief Creates and initializes a new t_hashtable
+ * Mallocs the struct, then sets size to param size and hashfunction to
+ * param hf. Then enough pointers get calloc'd to fit size. If malloc of hashtable
+ * or calloc of elements fail NULL gets returned.
+ * @param size Size of hashtable - that many elements pointer get alloc'd
+ * @param hf Pointer to hashfunction which should be used
+ * @return t_hashtable* pointer to malloc'd hashtable; NULL if malloc fail
+ */
 t_hashtable	*ft_hashtable_create(uint32_t size, t_hashfunction *hf)
 {
 	t_hashtable	*ht;
@@ -39,9 +55,21 @@ t_hashtable	*ft_hashtable_create(uint32_t size, t_hashfunction *hf)
 	ht->size = size;
 	ht->hash = hf;
 	ht->elements = ft_calloc(sizeof(t_env_var *), ht->size);
+	if (!ht->elements)
+	{
+		free(ht);
+		return (NULL);
+	}
 	return (ht);
 }
 
+/**
+ * @brief Deallocate given hashtable
+ * Loop through all elements in elements pointer.
+ * Free the string and the element.
+ * At the end free elements pointer and hashtable.
+ * @param ht Hashtable to destroy
+ */
 void	ft_hashtable_destroy(t_hashtable *ht)
 {
 	uint32_t	i;
