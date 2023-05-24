13a378c6e112575f6f0bb4c50fec22c0cb2aeb4c
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-24 12:45:06 +0200

    fix(ht.h): add includes, update return of fts

--- inc/hashtable.h
+++ inc/hashtable.h
@@ -6,16 +6,20 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:07:23 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/19 11:24:00 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/24 12:44:33 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HASHTABLE_H
 # define HASHTABLE_H
 
+# include <stdio.h>
 # include <stdint.h>
 # include <stdlib.h>
 # include <stdbool.h>
+
+# include "libft.h"
+# include "minishell_error.h"
 
 # define FNV_PRIME 0x100000001B3
 # define FNV_OFFSET 0xCBF29CE48422325UL
@@ -38,22 +42,15 @@ typedef struct s_hashtable {
 //hashtable_generate.c
 uint64_t	ft_hash_fnv1(const char *string, size_t len);
 t_hashtable	*ft_hashtable_create(uint32_t size, t_hashfunction *hf);
-void	ft_hashtable_destroy(t_hashtable *ht);
+void		ft_hashtable_destroy(t_hashtable *ht);
 
 //hashtable_utils.c
-static size_t	ft_hashtable_index(
-	t_hashtable *ht, const char *key, size_t keylen);
-bool	ft_hashtable_insert(t_hashtable *ht, const char *string, size_t keylen);
+t_error		ft_hashtable_insert(
+				t_hashtable *ht, const char *string, size_t keylen);
 t_env_var	*ft_hashtable_lookup(
-	t_hashtable *ht, const char *string, size_t keylen);
-const char	*ft_hashtable_delete(
-	t_hashtable *ht, const char *string, size_t keylen);
-void	ft_hashtable_print(t_hashtable *ht);
-
-
-
-
-
-
+				t_hashtable *ht, const char *string, size_t keylen);
+t_error		ft_hashtable_delete(
+				t_hashtable *ht, const char *string, size_t keylen);
+void		ft_hashtable_print(t_hashtable *ht);
 
 #endif
