64adbe0cbb044143561f142c37ede5b8d0fe6857
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-25 08:22:50 +0200

    temp: testing main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:15:13 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/24 14:10:22 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/24 19:15:49 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	main(void)
 	data = malloc(sizeof(*data));
 	if (!data)
 		ft_exit_failure(data, ERR_MALLOC);
-	data->env_table = ft_hashtable_create(HASHTABLE_SIZE, ft_hash_fnv1);
-	if (ft_import_env(data) != SUCCESS)
-		printf("NO");
+	if (ft_setup_env(data) != SUCCESS)
+		printf("NO\n");
+	ft_hashtable_print(data->env_table);
 	ft_hashtable_destroy(data->env_table);
 	free(data);
 }
