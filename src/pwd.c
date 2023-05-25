bcf041963cec97c07ba09a0946e363cb2df462b1
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-05-25 08:21:06 +0200

    fix: add comments

--- src/pwd.c
+++ src/pwd.c
@@ -6,27 +6,37 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:15:11 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/24 14:04:31 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/05/25 08:20:41 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_error	ft_create_pwd_value(char *pwd_value)
+/**
+ * @brief Create string with the present working directory
+ * Receives pointer to pointer to change value outside of function.
+ * Return is t_error, so it can't return pointer itself.
+ * Malloc string with 4096 and call getcwd for pwd.
+ * If path is longer than size, free and malloc twice the size.
+ * Repeat until path fits in array.
+ * @param pwd_value Pointer to pointer where to save string
+ * @return t_error Not SUCCESS if malloc or getcwd error.
+ */
+t_error	ft_create_pwd_value(char **pwd_value)
 {
 	size_t	size;
 
 	size = 4096;
 	while (1)
 	{
-		pwd_value = malloc(size);
-		if (pwd_value == NULL)
+		*pwd_value = malloc(size);
+		if (*pwd_value == NULL)
 			return (ERR_MALLOC);
-		if (getcwd(pwd_value, size) != NULL)
+		if (getcwd(*pwd_value, size) != NULL)
 			break ;
 		else
 		{
-			free(pwd_value);
+			free(*pwd_value);
 			if (errno == ERANGE)
 			{
 				size *= 2;
@@ -39,18 +49,28 @@ t_error	ft_create_pwd_value(char *pwd_value)
 	return (SUCCESS);
 }
 
-t_error	ft_create_pwd_str(char *pwd)
+/**
+ * @brief Creates a new env_var PWD=path/to/pwd
+ * Receives pointer to pointer to change value outside of function.
+ * Return is t_error, so it can't return pointer itself.
+ * Call ft_create_pwd_value to get right side of env_var.
+ * If error passes on the error.
+ * If success strjoin with left side.
+ * @param pwd Pointer to pointer where to save string.
+ * @return t_error Not SUCCESS if malloc or sub-call fail
+ */
+t_error	ft_create_pwd_env_str(char **pwd)
 {
 	t_error	err;
 	char	*pwd_value;
 
 	pwd_value = NULL;
-	err = ft_create_pwd_value(pwd_value);
+	err = ft_create_pwd_value(&pwd_value);
 	if (err != SUCCESS)
 		return (err);
-	pwd = ft_strjoin("PWD=", pwd_value);
+	*pwd = ft_strjoin("PWD=", pwd_value);
 	free(pwd_value);
-	if (!pwd)
+	if (*pwd == NULL)
 		return (ERR_MALLOC);
 	return (SUCCESS);
 }
