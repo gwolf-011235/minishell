017c9dfcf0a60dd55f0277b2f8aa22d5d98e508f
Gabriel <gwolf@student.42vienna.com>
2023-06-01 13:15:46 +0200

    fix: move in not found

--- src/prompt_replace_small.c
+++ src/prompt_replace_small.c
@@ -1,17 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prompt_replace_n.c                                 :+:      :+:    :+:   */
+/*   prompt_replace_small.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 12:05:43 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/01 12:16:03 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/01 12:18:29 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Create a standard string if replacement wasn't found.
+ *
+ * Strdup the macro UNKNOWN defined in minishell.h and save it.
+ *
+ * @param replacement Pointer pointer where to save string.
+ * @return t_error SUCCESS, ERR_EMPTY, ERR_MALLOC
+ */
+t_error	ft_prompt_replace_not_found(char **replacement)
+{
+	if (!replacement)
+		return (ERR_EMPTY);
+	*replacement = strdup(UNKNOWN);
+	if (!*replacement)
+		return (ERR_MALLOC);
+	return (SUCCESS);
+}
 /**
  * @brief Creates replacement string for prompt \n
  *
@@ -22,6 +39,7 @@
  * @param sym_tab symbol_table - not used.
  * @return t_error SUCCESS, ERR_EMPTY, ERR_MALLOC
  */
+
 t_error	ft_prompt_replace_n(char **replacement, t_hashtable sym_tab)
 {
 	t_error		err;
