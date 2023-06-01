d32d434277bcc0172b159f9f6900782d9b22d09e
Gabriel <gwolf@student.42vienna.com>
2023-06-01 16:45:39 +0200

    fix: add some stuff to compile

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:20:30 by gwolf             #+#    #+#             */
-/*   Updated: 2023/05/31 18:40:34 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/01 16:29:03 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,21 @@
 
 # define PS1_STD "$ "
 # define PS2_STD "> "
+# define UNKNOWN "UNKNOWN"
 
-typedef t_error	(*t_replace_ptr)(char **replacement);
+# define PROMPT_EXPAND_SUPPORTED "hnurw"
+# define PROMPT_EXPAND_H "\\h"
+# define PROMPT_EXPAND_N "\\n"
+# define PROMPT_EXPAND_U "\\u"
+# define PROMPT_EXPAND_R "\\r"
+# define PROMPT_EXPAND_W "\\w"
+
+typedef	int t_hashtable;
+typedef struct s_env_var {
+	char	*value;
+}	t_env_var;
+
+typedef t_error	(*t_replace_ptr)(char **replacement, t_hashtable sym_tab);
 
 //memory.c
 void	*ft_realloc(void *ptr, size_t new_size, size_t curr_size);
@@ -38,5 +51,20 @@ t_error	ft_replace_token(char **str, const char *token,
 //ft_string.c
 char	*ft_strcat(char *dst, const char *src);
 char	*ft_strncat(char *dst, const char *src, size_t n);
+
+//prompt_replace_small.c
+t_error	ft_prompt_replace_n(char **replacement, t_hashtable sym_tab);
+t_error	ft_prompt_replace_r(char **replacement, t_hashtable sym_tab);
+t_error	ft_prompt_replace_not_found(char **replacement);
+
+//prompt_replace_h.c
+t_error	ft_prompt_replace_h(char **replacement, t_hashtable sym_tab);
+t_error	ft_prompt_create_hostname(char **replacement, const char *str);
+
+//prompt_replace_u.c
+t_error	ft_prompt_replace_u(char **replacement, t_hashtable sym_tab);
+
+//prompt_replace_w.c
+t_error	ft_prompt_replace_w(char **replacement, t_hashtable sym_tab);
 
 #endif
