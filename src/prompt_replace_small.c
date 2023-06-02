b716e7a4950023bd2facc212543e2e9b3b750489
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-06-02 07:25:32 +0200

    feat: new function for empty string

--- src/prompt_replace_small.c
+++ src/prompt_replace_small.c
@@ -6,16 +6,16 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 12:05:43 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/01 12:18:29 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/02 07:22:34 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @brief Create a standard string if replacement wasn't found.
+ * @brief Create string if some $var for replacement wasn't found.
  *
- * Strdup the macro UNKNOWN defined in minishell.h and save it.
+ * Use ft_strdup() on macro UNKNOWN defined in minishell.h and save it.
  *
  * @param replacement Pointer pointer where to save string.
  * @return t_error SUCCESS, ERR_EMPTY, ERR_MALLOC
@@ -24,22 +24,22 @@ t_error	ft_prompt_replace_not_found(char **replacement)
 {
 	if (!replacement)
 		return (ERR_EMPTY);
-	*replacement = strdup(UNKNOWN);
+	*replacement = ft_strdup(UNKNOWN);
 	if (!*replacement)
 		return (ERR_MALLOC);
 	return (SUCCESS);
 }
+
 /**
- * @brief Creates replacement string for prompt \n
+ * @brief Creates replacement string for token \n.
  *
- * Just simply strdups '\n' and saves it.
+ * Just simply ft_strdup() '\n' and saves it.
  * Sym_tab is voided to use this ft in ft pointer.
  *
  * @param replacement Where to save string.
  * @param sym_tab symbol_table - not used.
  * @return t_error SUCCESS, ERR_EMPTY, ERR_MALLOC
  */
-
 t_error	ft_prompt_replace_n(char **replacement, t_hashtable sym_tab)
 {
 	t_error		err;
@@ -47,16 +47,16 @@ t_error	ft_prompt_replace_n(char **replacement, t_hashtable sym_tab)
 	if (!replacement || !sym_tab)
 		return (ERR_EMPTY);
 	(void)sym_tab;
-	*replacement = strdup("\n");
+	*replacement = ft_strdup("\n");
 	if (!*replacement)
 		return (ERR_MALLOC);
 	return (SUCCESS);
 }
 
 /**
- * @brief Creates replacement string for prompt \r
+ * @brief Creates replacement string for token \r.
  *
- * Just simply strdups '\r' and saves it.
+ * Just simply ft_strdup() '\r' and saves it.
  * Sym_tab is voided to use this ft in ft pointer.
  *
  * @param replacement Where to save string.
@@ -70,7 +70,30 @@ t_error	ft_prompt_replace_r(char **replacement, t_hashtable sym_tab)
 	if (!replacement || !sym_tab)
 		return (ERR_EMPTY);
 	(void)sym_tab;
-	*replacement = strdup("\r");
+	*replacement = ft_strdup("\r");
+	if (!*replacement)
+		return (ERR_MALLOC);
+	return (SUCCESS);
+}
+
+/**
+ * @brief Creates empty string for wrong token.
+ *
+ * Just simply ft_strdup() "" (empty string) and saves it.
+ * Sym_tab is voided to use this ft in ft pointer.
+ *
+ * @param replacement Where to save string.
+ * @param sym_tab symbol_table - not used.
+ * @return t_error SUCCESS, ERR_EMPTY, ERR_MALLOC
+ */
+t_error	ft_prompt_replace_empty(char **replacement, t_hashtable sym_tab)
+{
+	t_error		err;
+
+	if (!replacement || !sym_tab)
+		return (ERR_EMPTY);
+	(void)sym_tab;
+	*replacement = ft_strdup("");
 	if (!*replacement)
 		return (ERR_MALLOC);
 	return (SUCCESS);
