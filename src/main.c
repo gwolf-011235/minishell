a77ec47f16c368cafcd31b21ad950fa5064eca9b
Gabriel Wolf <gwolf@Gabriels-Z13>
2023-06-02 07:52:07 +0200

    feat: impl readline

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:15:13 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/02 17:06:26 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/02 17:07:06 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,24 @@ int	main(int argc, char **argv)
 	}
 	*/
 	return (0);
+	t_error	err;
+	char 	*input;
+
+	while (1)
+	{
+		err = ft_create_prompt(sym_tab, &prompt1, "PS1", PS1_STD);
+		if (err != SUCCESS)
+			//handle error
+		err = ft_create_prompt(sym_tab, &prompt2, "PS2", PS2_STD);
+		if (err != SUCCESS)
+			//handle error
+		input = readline(prompt1);
+		if (!input)
+			//handle no input e.g. break?
+		add_history(input);
+		//do stuff
+		free(input);
+		free(prompt1);
+		free(prompt2);
+	}
 }
