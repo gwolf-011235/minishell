0102aaa4b06b7b7af784ebf2916871109c5f7c02
Gabriel Wolf <gwolf@student.42vienna.com>
2023-06-24 11:37:13 +0200

    feat(error_code): err_syntax def as 2 like bash

--- inc/minishell_error.h
+++ inc/minishell_error.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 11:58:28 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/06 14:59:49 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/06/24 10:19:21 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,8 @@ typedef enum e_error {
 	ERR_NO_SHLVL, ///< ft_increment_shlvl didn't find SHLVL.
 	ERR_MAX_SHLVL, ///< maximum shell level was reached.
 	ERR_OUT_OF_BOUNDS, ///< A parameter has exceeded the allowed range.
-	ERR_WRONG_TOKEN ///< A prompt substitution was not recognized,
+	ERR_WRONG_TOKEN, ///< A prompt substitution was not recognized.
+	ERR_SYNTAX = 2, ///< Syntax error with quotes, pipes or redirect.
 }	t_error;
 
 
