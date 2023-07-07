86cd080d96e55e9e9ac46b2b8e27b5a8f6eb2f80
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-07 11:34:09 +0200

    feat: new num_elements in hashtable plus rm const

--- src/hashtable_utils2.c
+++ src/hashtable_utils2.c
@@ -6,13 +6,13 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 14:47:39 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/06 15:24:11 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/07 11:22:49 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hashtable.h"
 
-t_error	ft_hashtable_swap(t_hashtable *ht, const char *string, size_t keylen)
+t_error	ft_hashtable_swap(t_hashtable *ht, char *string, size_t keylen)
 {
 	t_env_var	*env_var;
 
@@ -21,7 +21,7 @@ t_error	ft_hashtable_swap(t_hashtable *ht, const char *string, size_t keylen)
 	env_var = ft_hashtable_lookup(ht, string, keylen);
 	if (!env_var)
 		return (ERR_HT_NO_SWAP);
-	free((void *)env_var->env_string);
+	free(env_var->env_string);
 	env_var->env_string = string;
 	env_var->value = string + keylen + 1;
 	return (SUCCESS);
