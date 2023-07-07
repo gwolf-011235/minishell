38832120b7dccfcba058858e4a4c10a1cb833423
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-07 12:10:39 +0200

    fix: update prototyp of ft_envp_detroy()

--- inc/env_envp.h
+++ inc/env_envp.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 08:11:46 by gwolf             #+#    #+#             */
-/*   Updated: 2023/06/28 18:25:56 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/07 12:10:03 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 # include "hashtable.h"
 
 t_error	ft_envp_create(t_hashtable *ht, char ***envp);
-t_error	ft_envp_destroy(char **envp);
+t_error	ft_envp_destroy(char ***envp);
 
 #endif
