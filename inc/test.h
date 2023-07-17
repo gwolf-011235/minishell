abc1ae1fe009e86e341d7d63c6b5abbd611bf459
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-17 21:28:10 +0200

    feat: add color macros

--- inc/test.h
+++ inc/test.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 10:32:12 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/14 19:45:36 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/17 20:20:34 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include "lexer_check_syntax.h"
 # include "expander.h"
 # include "env_envp.h"
+
+# define RESET "\033[0m"
+# define BOLD "\033[1m"
+# define BLACK "\033[30m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RED "\033[31m"
+# define BLUE "\033[34m"
 
 void	test_hashtable(void);
 void	test_replace_token(void);
