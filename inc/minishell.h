727035a4daafa2f9eac7dfdb77148a3e0a5b03f1
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-21 13:43:19 +0200

    refactor: get #defines out in own header

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:20:30 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/21 11:40:30 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/21 13:36:50 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,8 @@
 # include "minishell_error.h"
 # include "hashtable.h"
 # include "minishell_struct.h"
+# include "minishell_config.h"
 
-/* ====== DEFINITIONS ====== */
-
-# define HASHTABLE_SIZE 10
-# define MAX_SHLVL 10
-
-# define PS1_STD "$ "
-# define PS2_STD "> "
-# define UNKNOWN "UNKNOWN"
 
 # define PROMPT_EXPAND_SUPPORTED "hnurw"
 # define PROMPT_EXPAND_EMPTY "\\"
@@ -50,29 +43,11 @@
 
 typedef t_err		(*t_replace_ptr)(char **replacement, t_hashtable *sym_tab);
 
-/**
- * @brief Global environ which holds the passed environment.
- *
- * More portable solution than the usage of char *envp.
- */
-extern char	**environ;
+t_err	ft_env_setup(t_hashtable *env_table);
 
 //exit_failure.c
 void	ft_exit_failure(t_data *data, t_err exit_code);
 
-//init.c
-t_err	ft_env_setup(t_data *data);
-t_err	ft_env_import(t_data *data);
-t_err	ft_env_insert_pwd(t_data *data);
-t_err	ft_env_insert_shlvl(t_data *data);
-
-//pwd.c
-t_err	ft_create_pwd_value(char **pwd_value);
-t_err	ft_create_pwd_env_str(char **pwd);
-
-//shlvl.c
-t_err	ft_create_shlvl_env_str(char **shlvl);
-t_err	ft_increment_shlvl(t_data *data);
 
 //ft_memory.c
 void	*ft_realloc(void *ptr, size_t new_size, size_t curr_size);
---
+++ inc/minishell_config.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_config.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 11:46:31 by gwolf             #+#    #+#             */
+/*   Updated: 2023/07/21 11:47:14 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_CONFIG_H
+# define MINISHELL_CONFIG_H
+
+/* ====== DEFINITIONS ====== */
+
+# define HASHTABLE_SIZE 10
+# define MAX_SHLVL 10
+
+# define PS1_STD "$ "
+# define PS2_STD "> "
+# define UNKNOWN "UNKNOWN"
+
+#endif
