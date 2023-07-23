805e808bd646840b54dec7474042e14754f7b566
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-23 23:14:27 +0200

    feat: add prototype echo

--- inc/mod_builtin.h
+++ inc/mod_builtin.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 17:57:20 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/23 22:09:32 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/23 23:13:59 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,11 @@ t_err	ft_get_array_size(char **array, size_t *size);
 //error.c
 t_err	ft_cd_error(t_err err, char *oldpwd, char *path);
 t_err	ft_pwd_error(t_err err);
+
+//echo.c
+t_err	ft_skip_n_flags(char **argv, size_t *i, bool *newline);
+t_err	ft_write_args(char **argv, size_t *i);
+t_err	ft_echo(char **argv);
 
 //cd.c
 t_err	ft_set_env_var(t_hashtable *env_tab, char *key, char *string);
