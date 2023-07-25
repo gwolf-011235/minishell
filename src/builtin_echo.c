067cf6630439f6b41675843a705aaea5012679dd
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-25 17:12:21 +0200

    fix: add doxygen to cd

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,34 +6,27 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 22:47:19 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/23 23:25:51 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/25 17:11:47 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file builtin_echo.c
+ * @brief Implementation of builtin echo.
+ */
 #include "mod_builtin.h"
 
-t_err	ft_skip_n_flags(char **argv, size_t *i, bool *newline)
-{
-	while (argv[*i] && !ft_strncmp(argv[*i], "-n", ft_strlen(argv[*i])))
-	{
-		*newline = false;
-		(*i)++;
-	}
-	return (SUCCESS);
-}
-
-t_err	ft_write_args(char **argv, size_t *i)
-{
-	while (argv[*i])
-	{
-		ft_putstr_fd(argv[*i], 1);
-		if (argv[*i + 1] && argv[*i][0] != '\0')
-			write(1, " ", 1);
-		(*i)++;
-	}
-	return (SUCCESS);
-}
-
+/**
+ * @brief Print the given args on standard output.
+ *
+ * Check the size with ft_get_array_size().
+ * Skip over -n flags with ft_skip_n_flags().
+ * Write all remaining args with ft_write_args().
+ * Only print newline if no -n flags was encountered.
+ *
+ * @param argv NULL terminated args.
+ * @return t_err SUCCESS, ERR_EMPTY
+ */
 t_err	ft_echo(char **argv)
 {
 	size_t	i;
@@ -56,3 +49,49 @@ t_err	ft_echo(char **argv)
 		write (1, "\n", 1);
 	return (SUCCESS);
 }
+
+/**
+ * @brief Skip over all -n flags
+ *
+ * Increment the passed i as long as an "-n" string is found.
+ * It's possible to pass several -n flags even though they have no
+ * additional effect.
+ * If at least one -n flag is found, set bool newline to false.
+ *
+ * @param argv NULL terminated args.
+ * @param i Pointer to current index of array.
+ * @param newline Pointer to bool which can get switched.
+ * @return t_err SUCCESS
+ */
+t_err	ft_skip_n_flags(char **argv, size_t *i, bool *newline)
+{
+	while (argv[*i] && !ft_strncmp(argv[*i], "-n", ft_strlen("-n")))
+	{
+		*newline = false;
+		(*i)++;
+	}
+	return (SUCCESS);
+}
+
+/**
+ * @brief Write all args in array.
+ *
+ * Write arg at index i.
+ * If there is another arg, write a space.
+ *
+ * @param argv NULL temrinated args.
+ * @param i Pointer to current index of array.
+ * @return t_err SUCCESS
+ */
+t_err	ft_write_args(char **argv, size_t *i)
+{
+	while (argv[*i])
+	{
+		ft_putstr_fd(argv[*i], 1);
+		if (argv[*i + 1])
+			ft_putchar_fd(' ', 1);
+		(*i)++;
+	}
+	return (SUCCESS);
+}
+
