0e89043bc2d6843819bf88139dc740b7aaeee947
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-25 17:28:59 +0200

    fix: add doxygen to error

--- src/builtin_error.c
+++ src/builtin_error.c
@@ -6,12 +6,26 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 21:40:34 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/25 15:31:30 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/25 17:27:56 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file builtin_error.c
+ * @brief Error handling functions for builtins.
+ */
 #include "mod_builtin.h"
 
+/**
+ * @brief Handle error of ft_exit().
+ *
+ * Error if second arg is not numeric.
+ * Error if more than two args are given.
+ *
+ * @param err Error code.
+ * @param arg First arg given to ft_exit().
+ * @return t_err ERR_NONUM, ERR_EXIT.
+ */
 t_err	ft_exit_error(t_err err, char *arg)
 {
 	if (err == ERR_NONUM)
@@ -25,6 +39,16 @@ t_err	ft_exit_error(t_err err, char *arg)
 	return (err);
 }
 
+/**
+ * @brief Handle error of ft_export().
+ *
+ * Error if given name is not valid for env key.
+ * Error if malloc failed.
+ *
+ * @param err Error code.
+ * @param arg Current arg given to export.
+ * @return t_err ERR_INVALID_NAME, ERR_MALLOC.
+ */
 t_err	ft_export_error(t_err err, char *arg)
 {
 	if (err == ERR_INVALID_NAME)
@@ -38,6 +62,15 @@ t_err	ft_export_error(t_err err, char *arg)
 	return (err);
 }
 
+/**
+ * @brief Handle error of ft_pwd().
+ *
+ * Error if malloc failed.
+ * Error if getcwd failed.
+ *
+ * @param err Error Code.
+ * @return t_err ERR_MALLOC, ERR_CWD_FAIL.
+ */
 t_err	ft_pwd_error(t_err err)
 {
 	if (err == ERR_MALLOC)
@@ -47,6 +80,21 @@ t_err	ft_pwd_error(t_err err)
 	return (err);
 }
 
+/**
+ * @brief Handle error of ft_cd().
+ *
+ * If $PWD has already been saved in oldpwd free it.
+ * Error if more than two args are given.
+ * Error if $HOME was searched but not found.
+ * Error if malloc failed.
+ * Error if chdir failed, e.g. given path was not valid.
+ * In last case print exact error with perror.
+ *
+ * @param err Error code
+ * @param oldpwd Pointer to saved $PWD.
+ * @param path Arg given to chdir.
+ * @return t_err ERR_ARGCOUNT, ERR_NOT_FOUND, ERR_MALLOC, ERR_CHDIR_FAIL
+ */
 t_err	ft_cd_error(t_err err, char *oldpwd, char *path)
 {
 	if (oldpwd)
