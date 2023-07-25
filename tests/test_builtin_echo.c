8d5f733687e71e1fcd6ba211bf397242ed594b9b
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-25 17:12:42 +0200

    feat: add new echo tests with empty strings

--- tests/test_builtin_echo.c
+++ tests/test_builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 23:12:15 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/25 16:31:03 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/25 17:07:32 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,8 @@ void	test_builtin_echo(void)
 	exec_test_builtin_echo("Three -n flags", "-n", "-n", "-n");
 	exec_test_builtin_echo("No args", NULL, NULL, NULL);
 	exec_test_builtin_echo("Two -n flags, arg in the middle", "-n", "sneaky", "-n");
+	exec_test_builtin_echo("Empty string", "", NULL, NULL);
+	exec_test_builtin_echo("Empty string after arg", "First", "", NULL);
+	exec_test_builtin_echo("Empty string in the middle", "First", "", "Third");
 	test_teardown_argv();
 }
