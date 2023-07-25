882029289160cc1e3225b5d768a7511f9b2fbc93
Gabriel Wolf <gwolf@student.42vienna.com>
2023-07-25 13:35:49 +0200

    feat: added new unset tests

--- tests/test_builtin_unset.c
+++ tests/test_builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:17:27 by gwolf             #+#    #+#             */
-/*   Updated: 2023/07/25 13:25:07 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/07/25 13:35:31 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 
 extern t_hashtable	*g_symtab;
 extern char			**g_argv;
+
+void	exec_ft_unset(char *name, char *arg1, char *arg2, char *arg3)
+{
+	printf(BLUE"TEST: %s\n"RESET, name);
+	printf("1: %s\n2: %s\n3: %s\n\n", arg1, arg2, arg3);
+	g_argv[1] = arg1;
+	g_argv[2] = arg2;
+	g_argv[3] = arg3;
+	printf("\tRUN\n");
+	ft_unset(g_argv, g_symtab);
+	printf("\n");
+	printf("\tPRINT\n");
+	ft_hashtable_print(g_symtab);
+	printf("\n");
+
+}
 
 void	test_builtin_unset(void)
 {
@@ -26,8 +42,13 @@ void	test_builtin_unset(void)
 	ft_hashtable_insert(g_symtab, ft_strdup("uno=this"), 3);
 	ft_hashtable_insert(g_symtab, ft_strdup("dos=is"), 3);
 	ft_hashtable_insert(g_symtab, ft_strdup("tr3s=sparta"), 4);
+	ft_hashtable_insert(g_symtab, ft_strdup("quatro=!"), 6);
+	ft_hashtable_insert(g_symtab, ft_strdup("sneaky=boy"), 6);
 	ft_hashtable_print(g_symtab);
-	ft_unset(g_argv, g_symtab);
-	ft_hashtable_print(g_symtab);
-
+	exec_ft_unset("Unset one var", "uno", NULL, NULL);
+	exec_ft_unset("Unset three vars", "dos", "tr3s", "quatro");
+	exec_ft_unset("Unset not existing", "sneak", NULL, NULL);
+	exec_ft_unset("Unset whole env string", "sneaky=boy", NULL, NULL);
+	ft_hashtable_destroy(g_symtab);
+	test_teardown_argv();
 }
