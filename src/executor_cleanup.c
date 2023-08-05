1c7f316ffdb76cd393e5497efb99863557819476
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-08-05 17:44:02 +0200

    refactor: comment out unused functions

--- src/executor_cleanup.c
+++ src/executor_cleanup.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 17:11:28 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/05 16:17:06 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/08/05 17:00:37 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 /* This function terminates the program and displays an error message 
 according to the point in the program where the error happened. */
 
-void	terminate(char *s)
+/* void	terminate(char *s)
 {
 	if (errno == 0)
 		ft_putendl_fd(s, 2);
 	else
 		perror(s);
 	exit(1);
-}
+} */
 
 /* This function closes all open file descriptors, frees allocated memory
 space in the heap and unlinks any files at the current point. It then
 gives out an error message and terminates the program. */
 
-void	abort_mission(t_meta *meta, char *s)
+/* void	abort_mission(t_meta *meta, char *s)
 {
 	if (meta->here_doc)
 		unlink(".tmp_heredoc");
@@ -36,12 +36,12 @@ void	abort_mission(t_meta *meta, char *s)
 	ft_close(meta->fd_out);
 	free(meta->cmds);
 	terminate(s);
-}
+} */
 
 /* This function frees the cmd_paths and calls 'abort_mission' upon an
 error. */
 
-void	no_senor(t_meta *meta, char *s)
+/* void	no_senor(t_meta *meta, char *s)
 {
 	int	i;
 
@@ -50,12 +50,12 @@ void	no_senor(t_meta *meta, char *s)
 		free(meta->cmd_paths[i]);
 	free(meta->cmd_paths);
 	abort_mission(meta, s);
-}
+} */
 
 /* This function frees the arguments in the current command and 
 calls 'no senor' upon an error. */
 
-void	mamma_mia(t_meta *meta, char *s)
+/* void	mamma_mia(t_meta *meta, char *s)
 {
 	int	i;
 
@@ -64,12 +64,12 @@ void	mamma_mia(t_meta *meta, char *s)
 		free(meta->cmds[meta->i].arg[i]);
 	free(meta->cmds[meta->i].arg);
 	no_senor(meta, s);
-}
+} */
 
 /* This function closes remaining file descriptors and frees allocated 
 memory space. */
 
-void	cleanup(t_meta *meta)
+/* void	cleanup(t_meta *meta)
 {
 	int	i;
 
@@ -82,7 +82,7 @@ void	cleanup(t_meta *meta)
 		free(meta->cmd_paths[i]);
 	free(meta->cmd_paths);
 	free(meta->cmds);
-}
+} */
 
 /**
  * @brief Unlinks the temporary heredoc file created.
@@ -118,7 +118,7 @@ t_err	ft_close(int fd)
 It executes the closing for all pipes created in previous
 and including the current loop except if the the current
 loop is the last loop. */
-
+/* 
 void	pipinator(t_meta *meta)
 {
 	int	j;
@@ -129,13 +129,13 @@ void	pipinator(t_meta *meta)
 		if (j < meta->cmd_num - 1)
 			plug_pipes(meta, j);
 	}
-}
+} */
 
 /* This function closes the pipe ends (= file descriptors) of the
 specified pipe. */
-
+/* 
 void	plug_pipes(t_meta *meta, int i)
 {
 	ft_close(meta->cmds[i].fd[0]);
 	ft_close(meta->cmds[i].fd[1]);
-}
+} */
