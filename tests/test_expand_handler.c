309785fe00e72a0542580a7ad4330217226a3f47
Gabriel <gwolf@student.42vienna.com>
2023-08-06 18:11:11 +0200

    feat: new test spicy expand

--- tests/test_expand_handler.c
+++ tests/test_expand_handler.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 13:09:02 by gwolf             #+#    #+#             */
-/*   Updated: 2023/08/06 18:02:28 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/06 18:10:00 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,8 @@ void	test_expand_handler(void)
 	exec_ft_handle_arg("Var to expand and stuff after", "$TEST \"Hello      boy\"");
 	exec_ft_handle_arg("Don't split but rm quotes", "'I     no    split' \"Me   neither\"");
 	exec_ft_handle_arg("Don't split but rm quotes 2", "\"Me   neither\"");
+	ft_hashtable_insert(g_symtab, ft_strdup("S=s -la"), 1);
+	exec_ft_handle_arg("Spicy expand", "l$S");
 
 	printf(BLUE"**\tft_handle_heredoc\t**\n\n"RESET);
 	exec_ft_handle_heredoc("Remove quotes", "<< 'ahoy'");
