a30f95b749337350633e3f1d8c9ca1c641ec0dc5
sirui <sirui.qiu01@gmail.com>
2023-08-07 12:25:57 +0200

    test: pipes

--- tests/test_executor.c
+++ tests/test_executor.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:17:27 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/07 12:10:31 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/08/07 12:24:19 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,10 @@ static int	test_wrapper(char *testname, char *test)
 		printf("Total cmds: %d\n", cmd->cmd_num);
 		cmd = cmd->next;
 	}
-	if (local_err_count == 0)
+/* 	if (local_err_count == 0)
 		printf(GREEN"\nOK\n\n"RESET);
 	else
-		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count);
+		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count); */
 	return (local_err_count);
 }
 
@@ -78,14 +78,51 @@ static int	test_heredoc_wrapper(char *testname, char *test)
 		printf("Heredoc: %s\n", cmd->heredoc);
 		cmd = cmd->next;
 	}
-	if (local_err_count == 0)
+/* 	if (local_err_count == 0)
 		printf(GREEN"\nOK\n\n"RESET);
 	else
-		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count);
+		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count); */
 	return (local_err_count);
 }
 
 static int	test_scmd_wrapper(char *testname, char *test)
+{
+	int			local_err_count;
+	int			i;
+	t_tkn_list	*lst;
+	t_cmd		*cmd;
+	char		**envp;
+
+	local_err_count = 0;
+	i = 0;
+	lst = NULL;
+	cmd = NULL;
+	ft_env_setup(&g_symtab);
+	ft_envp_create(g_symtab, &envp);
+	printf("TEST: %s\n", testname);
+	printf("Command:%s\n\n", test);
+	ft_lex_input(&lst, test);
+	ft_parser(lst, &cmd);
+	ft_executor(cmd, envp);
+	printf("\n");
+/* 	while (cmd)
+	{
+		printf(GREEN"\ndelimiters:\n"RESET);
+		i = 0;
+		while (cmd->delims[i])
+			printf(GREEN"\t%s\n"RESET, cmd->delims[i++]);
+		printf("Infile: %d\n", cmd->fd_in);
+		printf("Heredoc: %s\n", cmd->heredoc);
+		cmd = cmd->next;
+	}
+	if (local_err_count == 0)
+		printf(GREEN"\nOK\n\n"RESET);
+	else
+		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count); */
+	return (local_err_count);
+}
+
+static int	test_pipe_wrapper(char *testname, char *test)
 {
 	int			local_err_count;
 	int			i;
@@ -153,10 +190,19 @@ static void	test_singlecmds(void)
 	test_scmd_wrapper("scmd with outfile in append mode", "cat infile >>outfile2");
 }
 
+static void	test_pipe(void)
+{
+	//test_pipe_wrapper("simple pipe", "ls -la | grep test");
+	//test_pipe_wrapper("pipe with append", "ls -la | grep test | wc -l >>outfile2");
+	//test_pipe_wrapper("pipe with individual infiles", "ls -la | <infile grep yo | <outfile wc -l");
+	test_pipe_wrapper("pipe with individual plural infiles", "ls -la | <infile <infile2 grep yo | <outfile <infile3 wc -m");
+}
+
 void	test_executor(void)
 {
 	printf(YELLOW"*******TEST_EXECUTOR*******\n\n"RESET);
 	//test_enum_cmds();
 	//test_heredoc();
-	test_singlecmds();
+	//test_singlecmds();
+	test_pipe();
 }
