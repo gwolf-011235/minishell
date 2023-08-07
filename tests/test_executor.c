3936bcb63eb33356c01a0af319e865ae760f6639
sirui <sirui.qiu01@gmail.com>
2023-08-07 11:45:54 +0200

    test: single cmds

--- tests/test_executor.c
+++ tests/test_executor.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:17:27 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/07 10:55:19 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/08/07 11:44:18 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,43 @@ static int	test_heredoc_wrapper(char *testname, char *test)
 	return (local_err_count);
 }
 
+static int	test_scmd_wrapper(char *testname, char *test)
+{
+	int			local_err_count;
+	int			i;
+	t_tkn_list	*lst;
+	t_cmd		*cmd;
+	char		**envp;
+
+	local_err_count = 0;
+	i = 0;
+	lst = NULL;
+	cmd = NULL;
+	ft_env_setup(&g_symtab);
+	ft_envp_create(g_symtab, &envp);
+	printf("TEST: %s\n", testname);
+	printf("Command:%s\n\n", test);
+	ft_lex_input(&lst, test);
+	ft_parser(lst, &cmd);
+	ft_executor(cmd, envp);
+	printf("\n");
+/* 	while (cmd)
+	{
+		printf(GREEN"\ndelimiters:\n"RESET);
+		i = 0;
+		while (cmd->delims[i])
+			printf(GREEN"\t%s\n"RESET, cmd->delims[i++]);
+		printf("Infile: %d\n", cmd->fd_in);
+		printf("Heredoc: %s\n", cmd->heredoc);
+		cmd = cmd->next;
+	}
+	if (local_err_count == 0)
+		printf(GREEN"\nOK\n\n"RESET);
+	else
+		printf(RED"\nKO:\t%d errors found\n\n"RESET, local_err_count); */
+	return (local_err_count);
+}
+
 static void	test_enum_cmds(void)
 {
 	test_wrapper("Five simple cmds", "<   infile ls -la | grep infile | wc -l  >outfile");
@@ -96,10 +133,24 @@ static void	test_heredoc(void)
 	test_heredoc_wrapper("infile followed by heredocs", "<<samba <infile <<gg <<yuhu grep bu");
 	test_heredoc_wrapper("piped heredocs", "<<sa cat | <<gg grep bu");
 }
+
+static void	test_singlecmds(void)
+{
+	//test_scmd_wrapper("scmd", "ls -la");
+	//test_scmd_wrapper("scmd", "touch mimimi");
+	//test_scmd_wrapper("scmd", "mkdir shubidu");
+	//test_scmd_wrapper("scmd", "cp -v mimimi shubidu");
+	//test_scmd_wrapper("scmd", "mv -v mimimi mumumu");
+	//test_scmd_wrapper("scmd", "rm mumumu");
+	//test_scmd_wrapper("scmd", "rm -r shubidu");
+	//test_scmd_wrapper("scmd", "rm -r shubidu");
+	test_scmd_wrapper("scmd with infile", "cat <infile");
+}
+
 void	test_executor(void)
 {
 	printf(YELLOW"*******TEST_EXECUTOR*******\n\n"RESET);
 	//test_enum_cmds();
-	test_heredoc();
-
+	//test_heredoc();
+	test_singlecmds();
 }
