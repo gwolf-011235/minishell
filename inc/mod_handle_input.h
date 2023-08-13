afdd6c96e78070745f5a4ca9945ddefe29581857
sirui <sirui.qiu01@gmail.com>
2023-08-13 22:27:43 +0200

    feat: add outfiles str array

--- inc/mod_handle_input.h
+++ inc/mod_handle_input.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mod_handle_input.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 12:50:47 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/12 00:08:33 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/13 22:27:19 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,25 @@
  *
  * Each node of the list contains information about the respective token. Not
  * all variables necessarily need to be populated.
- * @param exe Pointer to executable string.
  * @param args Pointer to argument string array.
+ * @param arg_pos Position of arg in arg array.
  * @param fd_in File descriptor of infile.
  * @param fd_out File descriptor of outfile.
- * @param delim Pointer to delimiter string.
- * @param append Boole value to indicate append mode.
+ * @param fd_pipe Integer array containing the read/write end of the pipe
+ * connecting this cmd to the next one.
+ * @param fd_prev_pipe Integer array containing the read/write end of the
+ * previous pipe.
+ * @param delims Pointer to delimiter string array.
+ * @param delim_pos Position of delimiter in delim array.
+ * @param heredoc Pointer to Heredoc name string.
+ * @param outfiles Pointer to outfile name string array.
+ * @param append Boolean value to indicate append mode.
+ * @param infile Boolean value to indicate infile(s) have been found.
+ * @param outfile Boolean value to indicate outfile(s) have been found.
  * @param index Index indicating position of command in the pipeline.
+ * @param cmd_num Total number of cmds in list.
+ * @param pid Process identifier for forking the cmd execution.
+ * @param next Pointer to next cmd.
  */
 typedef struct s_cmd
 {
@@ -53,6 +65,7 @@ typedef struct s_cmd
 	char			**delims;
 	int				delim_pos;
 	char			*heredoc;
+	char			**outfiles;
 	bool			append;
 	bool			infile;
 	bool			outfile;
