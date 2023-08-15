c6bf279e17d1aded66fb7171ac0db2a3723a713a
Gabriel <gwolf@student.42vienna.com>
2023-08-15 10:54:44 +0200

    fix: incl fd in ft_putendl call

--- src/error_temp.c
+++ src/error_temp.c
@@ -6,11 +6,12 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 09:42:58 by gwolf             #+#    #+#             */
-/*   Updated: 2023/08/15 10:00:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/15 10:50:30 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_error.h"
+#include "libft.h"
 #include <unistd.h>
 
 t_err	ft_err_write(int fd, char *str, char *msg)
@@ -35,7 +36,7 @@ t_err	ft_err_chdir(char *path, char *msg)
 	ret = chdir(path);
 	if (ret == -1)
 	{
-		ft_putstr_fd(msg);
+		ft_putstr_fd(msg, 2);
 		perror(path);
 		return (ERR_CHDIR_FAIL);
 	}
