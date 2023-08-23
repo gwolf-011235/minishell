f4d407e9e188981d58f7bea54672196d53303e19
Gabriel <gwolf@student.42vienna.com>
2023-08-23 16:42:33 +0200

    fix: move prototyp to correct place

--- inc/mod_env.h
+++ inc/mod_env.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 11:41:48 by gwolf             #+#    #+#             */
-/*   Updated: 2023/08/23 09:27:54 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/23 11:58:27 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ t_err	ft_env_setup(t_hashtable **env_table, char *argv_zero, t_buf *buf);
 t_err	ft_import_environ(t_hashtable *env_table);
 t_err	ft_copy_environ_str(t_hashtable *env_table, char *environ_str);
 t_err	ft_insert_env_prompt(t_hashtable *env_table, char opt);
-t_err	ft_insert_env_zero(t_hashtable *env_table, char *argv_zero);
 
 //env_pwd.c
 t_err	ft_create_pwd_value(t_buf *buf);
@@ -51,6 +50,7 @@ t_err	ft_insert_env_shlvl(t_hashtable *env_table);
 t_err	ft_get_pid_value(pid_t *pid);
 t_err	ft_create_env_pid(char **env_pid);
 t_err	ft_insert_env_pid(t_hashtable *env_table);
+t_err	ft_insert_env_zero(t_hashtable *env_table, char *argv_zero);
 
 //env_envp.c
 t_err	ft_envp_fill(t_hashtable *ht, char **envp, bool all);
