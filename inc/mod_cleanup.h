0f370f58682259151ddffb2f98e7cf45c0704639
Gabriel <gwolf@student.42vienna.com>
2023-08-26 11:28:28 +0200

    feat: new cleanup function for exit

--- inc/mod_cleanup.h
+++ inc/mod_cleanup.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 17:33:17 by sqiu              #+#    #+#             */
-/*   Updated: 2023/08/24 13:28:33 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/26 11:21:33 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 /* ====== Functions ====== */
 void	ft_free_str_arr(char **arr);
 void	ft_clean_after_loop(char *input, t_data *data);
+void	ft_clean_on_exit(t_data *data);
 
 //from MOD ENV
 t_err	ft_envp_destroy(char ***envp);
