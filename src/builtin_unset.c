c671f7097e10346f0fb85a6bb5025d23f63ec6a9
Gabriel <gwolf@student.42vienna.com>
2023-08-26 22:51:50 +0200

    fix: PS! and PS2 can be unsetted

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:07:27 by gwolf             #+#    #+#             */
-/*   Updated: 2023/08/26 18:46:37 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/08/26 22:50:48 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,19 @@ void	ft_unset(char **argv, t_hashtable *env_tab)
 {
 	size_t		i;
 	t_env_var	*env_var;
+	size_t		size;
 
 	i = 1;
 	while (argv[i])
 	{
+		size = 0;
+		if (ft_get_env_keylen(argv[i], &size) == ERR_INVALID_NAME)
+		{
+			i++;
+			continue ;
+		}
 		env_var = ft_hashtable_lookup(env_tab, argv[i], ft_strlen(argv[i]));
-		if (env_var && env_var->export)
+		if (env_var)
 			ft_hashtable_delete(env_tab, argv[i], ft_strlen(argv[i]));
 		i++;
 	}
